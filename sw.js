be4d307fcdfeeedcccb621275ba5b41386a03437
Ayush Majumdar <ayushmajumdar200@gmail.com>
2026-09-03 01:35:59 +0530

    Add service worker for asset caching ( sw.js)
    Implement service worker for caching assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'daily-routine-v05';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
